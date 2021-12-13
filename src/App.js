08b3cfc8d2092f5386682b59d0df9972f091cf31
addegbenga <addegbenga@gmail.com>
2021-12-13 04:27:06 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import RoutePages from "./pages/index";
 
 function App() {
   return (
-   <div>
-     <RoutePages/>
-   </div>
+    <>
+      <div className="hidden md:block">
+        <RoutePages />
+      </div>
+      <div className="md:hidden">
+        <h1>
+          Please switch to Tablet or Desktop mode Mobile Responsiveness will be
+          available shortly{" "}
+        </h1>
+      </div>
+    </>
   );
 }
 
