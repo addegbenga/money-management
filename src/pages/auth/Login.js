0171c7ad78fe3e3673beae02dd0c53674fb358f9
addegbenga <addegbenga@gmail.com>
2021-12-13 14:06:57 -0500

    forms responsiveness

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -2,20 +2,23 @@ import React from "react";
 
 export default function Login() {
   return (
-    <div className=" py-32  md:px-10 lg:px-20">
-      <div className=" bg-white ">
+    <div
+     
+      className=" py-32 flex  justify-center  md:px-10 lg:px-20"
+    >
+      <div className=" bg-white md:py-8 md:rounded  md:shadow-lg lg:w-1/2 ">
         <h1 className="secondary-text text-center text-2xl font-semibold">
           Sign in
         </h1>
         <div className="mt-10">
-          <form className="border-t px-5 py-10 grid gap-4 ">
+          <form className="border-t border-opacity-50 px-5 py-10 grid gap-4 ">
             <input
-              className="border-2 rounded p-3"
+              className="border-2 md:border rounded p-3"
               type="text"
               placeholder="Email address"
             />
             <input
-              className="border-2 rounded p-3"
+              className="border-2 md:border rounded p-3"
               type="password"
               placeholder="Password"
             />
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -3,32 +3,32 @@ import { Link } from "react-router-dom";
 
 export default function Register() {
   return (
-    <div className=" py-32  md:px-10 lg:px-20">
-      <div className=" bg-white ">
+    <div className=" py-32 flex  justify-center  md:px-10 lg:px-20">
+      <div className="  bg-white md:py-8 md:rounded  md:shadow-lg lg:w-1/2 ">
         <h1 className="secondary-text text-center text-2xl font-semibold">
           Sign up
         </h1>
         <div className="mt-10">
-          <form className="border-t px-5 py-10 grid gap-4 ">
+          <form className="border-t border-opacity-50 px-5 py-10 grid gap-4 ">
             <div className="grid grid-cols-2 gap-3">
               <input
-                className="border-2 rounded p-3"
+                className="border-2 md:border rounded p-3"
                 type="text"
                 placeholder="First name"
               />
               <input
-                className="border-2 rounded p-3"
+                className="border-2 md:border rounded p-3"
                 type="text"
                 placeholder="Last name"
               />
             </div>
             <input
-              className="border-2 rounded p-3"
+              className="border-2 md:border rounded p-3"
               type="text"
               placeholder="Email address"
             />
             <input
-              className="border-2 rounded p-3"
+              className="border-2 md:border rounded p-3"
               type="password"
               placeholder="Password"
             />
